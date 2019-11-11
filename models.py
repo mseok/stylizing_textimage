5a2b3db3db8754b1213dd18106f41f16194194e3
mseok <mshmjp02@gmail.com>
2019-11-12 05:42:04 +0900

    initial commit

---
+++ models.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def forward(self, input):
+
+        return input
