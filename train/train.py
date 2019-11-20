837f7fbc27de82b954d08f3ed1b63fb592c98a3f
mseok <mshmjp02@gmail.com>
2019-11-20 23:54:33 +0900

    fixed bugs

--- train/train.py
+++ train/train.py
@@ -26,61 +26,57 @@ import torch.utils.data
 import torch.utils.data.distributed
 
 
-def train(generator, discriminator, dataset, gen_criterion, dis_criterion,
-          gen_optimizer, dis_optimizer, real, fake, args):
+def train(generator, discriminator, target, source, glyph, gen_criterion,
+          dis_criterion, gen_optimizer, dis_optimizer, real, fake, args):
     generator.train()
     discriminator.train()
 
-    for data in dataset:
-        for i, (glyph_input, source_input, target) in enumerate(data):
-            gen_optimizer.zero_grad()
-            dis_optimizer.zero_grad()
+    gen_optimizer.zero_grad()
+    dis_optimizer.zero_grad()
 
-            # glyph_input = torch.tensor(glyph_input, dtype=torch.float32, requires_grad=True)
-            # source_input = torch.tensor(source_input, dtype=torch.float32, requires_grad=True)
-            # target = torch.tensor(target)
+    # glyph = torch.tensor(glyph, dtype=torch.float32, requires_grad=True)
+    # source = torch.tensor(source, dtype=torch.float32, requires_grad=True)
+    # target = torch.tensor(target)
             
-            if args.gpu:
-                glyph_input = glyph_input.cuda()
-                source_input = source_input.cuda()
-                target = target.cuda()
+    if args.gpu:
+        glyph = glyph.cuda()
+        source = source.cuda()
+        target = target.cuda()
 
-            generated_target = generator(source_input, glyph_input)
-            gen_loss = gen_criterion(generated_target, target)
-            gan_loss = dis_criterion(discriminator(generated_target), fake) \
-                    + dis_criterion(discriminator(target), real)
-            
-            total_loss = gen_loss + gan_loss
-            total_loss.backward()
-            gen_optimizer.step()
-            dis_optimizer.step()
+    generated_target = generator(source, glyph)
+    gen_loss = gen_criterion(generated_target, target)
+    gan_loss = dis_criterion(discriminator(generated_target), fake) \
+            + dis_criterion(discriminator(target), real)
+    
+    total_loss = gen_loss + gan_loss
+    total_loss.backward()
+    gen_optimizer.step()
+    dis_optimizer.step()
 
-            return total_loss
+    return total_loss
 
-def val(generator, discriminator, dataset, gen_criterion, dis_criterion, real, fake, args):
+def val(generator, discriminator, target, source, glyph,
+        gen_criterion, dis_criterion, real, fake, args):
     generator.eval()
     discriminator.eval()
 
-    for data in dataset:
-        for i, (glyph_input, source_input, target) in enumerate(data):
-
-            # glyph_input = torch.tensor(glyph_input, dtype=torch.float32, requires_grad=True)
-            # source_input = torch.tensor(source_input, dtype=torch.float32, requires_grad=True)
-            # target = torch.tensor(target)
+    # glyph = torch.tensor(glyph, dtype=torch.float32, requires_grad=True)
+    # source = torch.tensor(source, dtype=torch.float32, requires_grad=True)
+    # target = torch.tensor(target)
             
-            if args.gpu:
-                glyph_input = glyph_input.cuda()
-                source_input = source_input.cuda()
-                target = target.cuda()
+    if args.gpu:
+        glyph = glyph.cuda()
+        source = source.cuda()
+        target = target.cuda()
 
-            generated_target = generator(source_input, glyph_input)
-            gen_loss = gen_criterion(generated_target, target)
-            gan_loss = dis_criterion(discriminator(generated_target), fake) \
-                    + dis_criterion(discriminator(target), real)
-            
-            total_loss = gen_loss + gan_loss
+    generated_target = generator(source, glyph)
+    gen_loss = gen_criterion(generated_target, target)
+    gan_loss = dis_criterion(discriminator(generated_target), fake) \
+            + dis_criterion(discriminator(target), real)
+    
+    total_loss = gen_loss + gan_loss
 
-            return total_loss
+    return total_loss
 
 
 def save_checkpoint(state_dict, is_best, model_type, filename='checkpoint.pt'):
@@ -145,30 +141,36 @@ if __name__ == "__main__":
     3. get output_source by concating source_list which is selected in (2)
        alphabets from input data(1)
     """
-    for batch_idx, (data, _) in enumerate(load_dataset(args, True)):
-        data = data # b * 3 * 64 * (64*26)
+    for batch_idx, (data, _) in enumerate(load_dataset(args, color=True)):
+        target_input = data # b * 3 * 64 * (64*26)
         position_list = alphabet_position('tlqkf')
+        
         source_list = []
         for p in position_list:
             source_list.append(data[:,:,:,64*(p-1):64*p])
-        output_source = torch.cat(source_list, dim=3)
-
+        source_input = torch.cat(source_list, dim=3)
+    
     # pretrained model for content selector
-    pt = models.vgg16(pretrained=True).features.eval()
-    content_selector, content_losses = transfer_model(pt, output_source)
+    # pt = models.vgg16(pretrained=True).features.eval()
+    
+    for batch_idx, (data, _) in enumerate(load_dataset(args, color=False)):
+        glyph_input = data # b * 3 * 64 * (64*26)
+        break
 
-    content_selector(output_source.transpose(2,3))
+    # content_selector, content_losses = transfer_model(pt, output_source)
+
+    # content_selector(output_source.transpose(2,3))
     
     # b * 64 * (64 * 26) * 3 glyph input
     # b * 64 * (64 * 5) * 3 source input
-    glyph_input = torch.randn(4, 3, 64, 64*26)
-    source_input = torch.randn(4, 3, 64, 64*5)
-    target = torch.randn(4, 3, 64, 64*26)
+    # glyph_input = torch.randn(4, 3, 64, 64*26)
+    # source_input = torch.randn(4, 3, 64, 64*5)
+    # target = torch.randn(4, 3, 64, 64*26)
     print("======= Finished Loading Datasets =======")
 
     # GENERATOR
     generator = Generator(args.latent_dim)
-    generator_loss = nn.L1loss()
+    generator_loss = nn.L1Loss()
     gen_optimizer = optim.Adam(generator.parameters(), lr=args.learning_rate)
 
     # ADVERSARIAL
@@ -198,19 +200,35 @@ if __name__ == "__main__":
     best_loss = 99999
     with SummaryWriter() as writer:
         for epoch in range(args.epoch):
-            train_loss = train(generator, discriminator, train_dataset,
-                               generator_loss, discriminator_loss,
-                               gen_optimizer, dis_optimizer, real, fake, args)
+            epoch_train_loss = []
+            for batch_idx, (data, _) in enumerate(load_dataset(args, color=True)):
+                target_input = data # b * 3 * 64 * (64*26)
+                position_list = alphabet_position('tlqkf')        
+                source_list = []
+                for p in position_list:
+                    source_list.append(data[:,:,:,64*(p-1):64*p])
+                source_input = torch.cat(source_list, dim=3)
+                # glyph_input = select(source_input)
+                loss = train(generator, discriminator, target_input, source_input,
+                             glyph_input, generator_loss, discriminator_loss,
+                             gen_optimizer, dis_optimizer, real, fake, args)
+                epoch_train_loss.append(train)
+                print("epoch: {}, cycle: {}, loss: {}".format(epoch, batch_idx, loss))
+            train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
             writer.add_scalar('train/loss', train_loss.item(), epoch)
             
-            val_loss = val(generator, discriminator, val_dataset,
-                           generator_loss, discriminator_loss,
-                           real, fake, args)
-            writer.add_scalar('val/loss', val_loss.item(), epoch)
-
-            is_best = val_loss <= best_loss
+            is_best = train_loss <= best_loss
             if is_best:
-                best_loss = val_loss
+                best_loss = train_loss
+
+            # val_loss = val(generator, discriminator, val_dataset,
+            #                generator_loss, discriminator_loss,
+            #                real, fake, args)
+            # writer.add_scalar('val/loss', val_loss.item(), epoch)
+
+            # is_best = val_loss <= best_loss
+            # if is_best:
+            #     best_loss = val_loss
             
             save_checkpoint({
                 'epoch': epoch,
@@ -219,12 +237,12 @@ if __name__ == "__main__":
                 'best_loss': best_loss,
             }, is_best, 'generator')
             
-            save_checkpoint({
-                'epoch': epoch,
-                'model': discriminator.state_dict(),
-                'optimizer': dis_optimizer.state_dict(),
-                'best_loss': best_loss,
-            }, is_best, 'discriminator')
+            # save_checkpoint({
+            #     'epoch': epoch,
+            #     'model': discriminator.state_dict(),
+            #     'optimizer': dis_optimizer.state_dict(),
+            #     'best_loss': best_loss,
+            # }, is_best, 'discriminator')
 
     # train dataloader -> glyph 26, source 5
     # via model -> generate total all 26 alphabets
