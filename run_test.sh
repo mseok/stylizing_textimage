776f9916083d382d6ff46f8deac55ddb284eb935
mseok <mshmjp02@gmail.com>
2019-11-27 21:38:41 +0900

    output checking

--- run_test.sh
+++ run_test.sh
@@ -9,4 +9,4 @@ COLOR_PATH="datasets/Capitals_colorGrad64/train"
 NONCOLOR_PATH="datasets/Capitals64/BASE"
 BATCH_SIZE=1
 
-python3 test/test.py --input_location $INPUT_LOCATION --pretrained_location $PRETRAINED_LOCATION --output_folder $OUTPUT_FOLDER --output_name $OUTPUT_NAME --latent_dim $LATENT_DIM --color_path $COLOR_PATH --noncolor_path $NONCOLOR_PATH --batch_size $BATCH_SIZE
+CUDA_VISIBLE_DEVICES=2 python3 test/test.py --input_location $INPUT_LOCATION --pretrained_location $PRETRAINED_LOCATION --output_folder $OUTPUT_FOLDER --output_name $OUTPUT_NAME --latent_dim $LATENT_DIM --color_path $COLOR_PATH --noncolor_path $NONCOLOR_PATH --batch_size $BATCH_SIZE
